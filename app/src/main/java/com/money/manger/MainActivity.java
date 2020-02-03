493883475274f12794271f02a56ea5160590faff
prakash-android <prakash98.techzar@gmail.com>
2020-02-03 16:56:11 +0530

    added date picker

--- app/src/main/java/com/money/manger/MainActivity.java
+++ app/src/main/java/com/money/manger/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -20,55 +23,75 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     Calendar myCalendar;
-    @BindView(R.id.setDate)
-    EditText dateEditText;
+    @BindView(R.id.datePicker)
+    DatePicker datePicker;
+
 
     @BindView(R.id.root_layout)
     ConstraintLayout constraintLayout;
+    String selectedDate = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
     }
 
 
-    @OnClick(R.id.setDate)
-    public void selectDate(){
-         myCalendar = Calendar.getInstance();
-         int y = myCalendar.get(Calendar.YEAR);
-         int m = myCalendar.get(Calendar.MONTH);
-         int d = myCalendar.get(Calendar.DAY_OF_MONTH);
 
-        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-
-            @Override
-            public void onDateSet(DatePicker view, int year,
-                                  int monthOfYear, int dayOfMonth) {
-
-                dateEditText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
-
-            }
-        }
-        ,
-                y,m,d);
-        dialog.show();
+    public String getCurrentDate(){
+        StringBuilder builder=new StringBuilder();
+        builder.append((datePicker.getMonth() + 1)+"/");
+        builder.append(datePicker.getDayOfMonth()+"/");
+        builder.append(datePicker.getYear());
+        return builder.toString();
     }
 
     @OnClick(R.id.btn)
     public void nextButton(){
-        // validate n move to next screen
-        if(dateEditText.getText().toString().equals("select date")) {
-            Snackbar snackbar = Snackbar
-                    .make(constraintLayout, "select the date", Snackbar.LENGTH_LONG);
-            snackbar.show();
-        } else {
-            Intent i = new Intent(this, ExpensesActivity.class);
-            startActivity(i);
-        }
+        Intent i = new Intent(this, ExpensesActivity.class);
+        i.putExtra("date", "" + getCurrentDate());
+        Toast.makeText(this,getCurrentDate(),Toast.LENGTH_LONG).show();
+        startActivity(i);
     }
 
-
-
 }
+
+
+
+//    @OnClick(R.id.setDate)
+//    public void selectDate(){
+//         myCalendar = Calendar.getInstance();
+//         int y = myCalendar.get(Calendar.YEAR);
+//         int m = myCalendar.get(Calendar.MONTH);
+//         int d = myCalendar.get(Calendar.DAY_OF_MONTH);
+//
+//        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+//
+//            @Override
+//            public void onDateSet(DatePicker view, int year,
+//                                  int monthOfYear, int dayOfMonth) {
+//
+//                dateEditText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+//
+//            }
+//        }
+//        ,
+//                y,m,d);
+//        dialog.show();
+//    }
+
+//    @OnClick(R.id.btn)
+//    public void nextButton(){
+//        // validate n move to next screen
+//        if(dateEditText.getText().toString().equals("select date")) {
+//            Snackbar snackbar = Snackbar
+//                    .make(constraintLayout, "select the date", Snackbar.LENGTH_LONG);
+//            snackbar.show();
+//        } else {
+//            Intent i = new Intent(this, ExpensesActivity.class);
+//            startActivity(i);
+//        }
+//    }

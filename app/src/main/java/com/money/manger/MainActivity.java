09525c81cf74dafde1999e89f9570b8b6727f78a
prakash-android <prakash98.techzar@gmail.com>
2020-02-03 13:26:36 +0530

    added date picker

--- app/src/main/java/com/money/manger/MainActivity.java
+++ app/src/main/java/com/money/manger/MainActivity.java
@@ -1,13 +1,51 @@
 package com.money.manger;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    Calendar myCalendar;
+    @BindView(R.id.setDate)
+    EditText dateEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.setDate)
+    public void selectDate(){
+         myCalendar = Calendar.getInstance();
+         int y = myCalendar.get(Calendar.YEAR);
+         int m = myCalendar.get(Calendar.MONTH);
+         int d = myCalendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year,
+                                  int monthOfYear, int dayOfMonth) {
+
+                dateEditText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+            }
+        }
+        ,
+                y,m,d);
+        dialog.show();
     }
 }

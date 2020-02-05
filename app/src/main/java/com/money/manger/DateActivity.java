43a2bad078f4b7f55b3b8517a029f58af722e271
prakash-android <prakash98.techzar@gmail.com>
2020-02-05 16:48:04 +0530

    add custom cal view with simpledateformats. added theme colors

---
+++ app/src/main/java/com/money/manger/DateActivity.java
@@ -0,0 +1,151 @@
+package com.money.manger;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class DateActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.calendarView)
+    CalendarView calenderView;
+
+    @BindView(R.id.dateTextView)
+    TextView dateTextView;
+
+
+    @BindView(R.id.root_layout)
+    RelativeLayout rootLayout;
+
+    String selectedDate = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date);
+        ButterKnife.bind(this);
+
+        setupListeners();
+    }
+
+
+
+    //date formatter with month in 3 letters
+    public String dateWithMonthInLetters(String inputString) {
+        String formattedDate = "";
+
+        try {
+            SimpleDateFormat originalFormat =
+                   new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+            SimpleDateFormat targetFormat = new SimpleDateFormat("dd MMM yyyy");
+            Date date = originalFormat.parse(inputString);
+            formattedDate = targetFormat.format(date);
+        } catch (Exception e){
+            Log.e("Exception", "" + e.getMessage());
+        }
+        return formattedDate;
+    }
+
+    // get double digits in date
+    public String doubleDigitNumber(int Date) {
+
+        String initialNumber = "";
+
+        if (Date < 10) {
+            initialNumber = "0" + Date;
+        } else {
+            initialNumber = "" + Date;
+        }
+
+        return initialNumber;
+    }
+
+
+
+    public void setupListeners(){
+        // Get Current Date as default
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+        selectedDate = (doubleDigitNumber(mDay) + "/" + doubleDigitNumber((mMonth + 1)) + "/" + mYear);
+//        dateTextView.setText(date);
+
+        calenderView.setOnDateChangeListener( new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                selectedDate = (doubleDigitNumber(dayOfMonth) + "/" + doubleDigitNumber((month + 1)) + "/" + year);
+                dateTextView.setText(selectedDate);
+            }
+        });
+    }
+
+
+    @OnClick(R.id.btn)
+    public void nextButton(){
+        dateTextView.setText(selectedDate);
+        Intent i = new Intent(this, ExpensesActivity.class);
+        i.putExtra("date", "" + selectedDate);
+        Toast.makeText(this,dateWithMonthInLetters(selectedDate), Toast.LENGTH_LONG).show();
+        startActivity(i);
+    }
+
+
+
+
+
+}
+
+
+
+/*
+    @OnClick(R.id.setDate)
+    public void selectDate(){
+         myCalendar = Calendar.getInstance();
+         int y = myCalendar.get(Calendar.YEAR);
+         int m = myCalendar.get(Calendar.MONTH);
+         int d = myCalendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year,
+                                  int monthOfYear, int dayOfMonth) {
+
+                dateEditText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+            }
+        }
+        ,
+                y,m,d);
+        dialog.show();
+    }
+
+    @OnClick(R.id.btn)
+    public void nextButton(){
+        // validate n move to next screen
+        if(dateEditText.getText().toString().equals("select date")) {
+            Snackbar snackbar = Snackbar
+                    .make(constraintLayout, "select the date", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        } else {
+            Intent i = new Intent(this, ExpensesActivity.class);
+            startActivity(i);
+        }
+    }*/

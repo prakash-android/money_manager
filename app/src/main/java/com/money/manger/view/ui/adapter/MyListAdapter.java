27bcdaa8b13679f28ba8dbebfa8fef99f62f7534
prakash-android <prakash98.techzar@gmail.com>
2020-02-13 12:34:39 +0530

    overflow menu added in adapter. design fix

--- app/src/main/java/com/money/manger/view/ui/adapter/MyListAdapter.java
+++ app/src/main/java/com/money/manger/view/ui/adapter/MyListAdapter.java
@@ -3,17 +3,20 @@ package com.money.manger.view.ui.adapter;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.RecyclerView;
 import com.money.manger.R;
 import com.money.manger.model.MyListData;
+import com.money.manger.view.database.DbHelper;
 
 import java.util.ArrayList;
 
@@ -22,11 +25,14 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
 
     private ArrayList<MyListData> listdata;
     private Context mContext;
+    DbHelper dbHelper;
+
 
     // RecyclerView recyclerView;
     public MyListAdapter(Context context , ArrayList<MyListData> listdata) {
         this.listdata = listdata;
         mContext = context;
+        dbHelper = new DbHelper(mContext);
     }
 
     @Override
@@ -48,13 +54,30 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
         final MyListData myListData = listdata.get(position);
         holder.textViewItem.setText(listdata.get(position).getItem());
         holder.textViewAmt.setText(listdata.get(position).getAmt());
-        holder.imageButtonOverflow.setOnClickListener(new View.OnClickListener() {
+
+
+
+        holder.overflowImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 PopupMenu popup = new PopupMenu(mContext, view);
                 MenuInflater inflater = popup.getMenuInflater();
                 inflater.inflate(R.menu.overflow_menu, popup.getMenu());
-                popup.setOnMenuItemClickListener(new MyMenuItemClickListener(mContext, myListData, listdata, position));
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_edit:
+                                editAction(listdata.get(position).getId(), listdata.get(position).getItem(), listdata.get(position).getAmt());
+                                return true;
+                            case R.id.action_delete:
+                                deleteAction(position, listdata.get(position).getId());
+                                return true;
+                            default:
+                        }
+                        return false;
+                    }
+                });
                 popup.show();
             }
         });
@@ -65,15 +88,37 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
         return listdata.size();
     }
 
+
+
+    // removes the row
+    public void deleteAction(int position, int id) {
+        boolean resultQuery = false;
+        resultQuery = dbHelper.deleteItem(id);
+        if(resultQuery) {
+            listdata.remove(position);
+            notifyItemRemoved(position);
+            Toast.makeText(mContext, "expenses deleted", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(mContext, "error occurred", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //edit the values
+    public void editAction(int id, String item, String amt) {
+        Toast.makeText(mContext,  item + " "+ amt + " " + id, Toast.LENGTH_SHORT).show();
+    }
+
+
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView textViewItem;
         public TextView textViewAmt;
-        public ImageButton imageButtonOverflow;
+        public ImageButton overflowImageButton;
         public ViewHolder(View itemView) {
             super(itemView);
             this.textViewItem = (TextView) itemView.findViewById(R.id.textViewItem);
             this.textViewAmt = (TextView) itemView.findViewById(R.id.textViewAmount);
-            this.imageButtonOverflow = (ImageButton) itemView.findViewById(R.id.overflow_icon);
+            this.overflowImageButton = (ImageButton) itemView.findViewById(R.id.overflow_imagebutton);
         }
     }
 }
